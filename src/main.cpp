687ed2fe1d0147ddc6a81c9ae21421d0ccbc8fed
schmynth <service-noreply@gmx.de>
2024-10-09 08:50:07 +0200

    removed chef files /usr/bin/env /bin/sh /tmp/Microsoft-MIEngine-Cmd-cwix2by4.dpb

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,11 @@
 
 using namespace std;
 
-Chef chef;
-    ItalianChef italianChef;
-
-
-
-
-
-enum PotPins
-{
-    filter_attack_pin = 0,
-    filter_decay_pin = 1,
-};
-
 
 int main()
 {
-    int y;
-    chef.makeSpecialDish();
-    italianChef.makePasta();
-    italianChef.makeSpecialDish();
-    cout << "input a number: " << endl;
-    cin >> y;
-    cout << "your number was: "<< y << endl;
-    cout << "Filter Attack Pin is pin no.: " << filter_attack_pin << endl;
-    cout << "Do i have to make every time?" << endl;
+    cout << "Hello World!" << endl;
+    return 0;
 }
 
 
