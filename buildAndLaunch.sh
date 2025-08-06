d956d74c873dc16439dc7f700f50b93077ed43b1
schmynth <sebastian.schmidt@electronxx.de>
2025-08-06 16:05:55 +0200

    FIX: add buildAndLaunch.sh and tweak it for basic cpp

---
+++ buildAndLaunch.sh
@@ -0,0 +1,22 @@
+!/bin/bash
+
+scrDir=$(pwd)
+ProjectName=$(grep "project" CMakeLists.txt | sed 's/project(//' | sed 's/.$//')
+
+if [ $1 = "--remove-build" ]; then
+	rm -rf build/plugin
+	echo "removed plugin build"
+fi
+
+if [ $2 = "--all" ]; then
+	rm -rf build/
+	echo "removed whole build directory"
+fi
+
+# build project
+echo "configuring project with CMake..."
+cmake -S . -B build
+echo "building project..."
+cmake --build build
+
+./build/hello
